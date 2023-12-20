cae9b62d26066a9e7b9ad4a63d2dff5af559841a
smartonc3 <smartonc3@inbox.lv>
2023-12-20 10:42:51 +0000

    C#-Hello6

---
+++ C3_Hello5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+Console.WriteLine("Урф, это Маша ! ");
+}
+else
+{
+Console.Write("Приыет, ");
+Console.WriteLine(username);
+}
+
+
+
